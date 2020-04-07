f2633dca0b969269c52ac51499f0ab41e3c16b89
Christin Bergmann <christin.bergmann@gmx.net>
2020-04-07 12:42:24 +0200

    missed curly bracket

--- weather.js
+++ weather.js
@@ -26,77 +26,78 @@ function searchWeather(searchTerm) {
             init(result);
         })
 
-    });
-
-    function init(dataServer) {
-        console.log(dataServer)
-        switch (dataServer.weather[0].main) {
-            case "Clear":
-                document.body.style.backgroundImage = "url('./pictures/clear1.jpg')";
-                break;
-
-            case "Clouds":
-                document.body.style.backgroundImage = "url('./pictures/cloudy.jpg')";
-                break;
-
-            case "Rain":
-            case "Drizzle":
-                document.body.style.backgroundImage = "url('./pictures/rainy.jpg')";
-                break;
-            case "Mist":
-                document.body.style.backgroundImage = "url('./pictures/mist.jpg')";
-                break;
-
-            case "Snow":
-                document.body.style.backgroundImage = "url('./pictures/snowy.jpg')";
-                break;
-
-            case "Thunderstorm":
-                document.body.style.backgroundImage = "url('./pictures/stormy.jpg')";
-                break;
-
-            default:
-                // document.body.style.backgroundImage = "url('./pictures/default1.jpg')";
-                break;
-        }
-        let weatherDescriptionMain = document.getElementById("weatherDescriptionMain")
-        let temperatureElement = document.getElementById("temperatureMain")
-        let humidityElement = document.getElementById("humidity")
-        let windElement = document.getElementById("wind")
-        let cityName = document.getElementById("cityName")
-        let weatherIcon = document.getElementById("iconImg")
-
-        weatherIcon.src = "http://openweathermap.org/img/wn/" + dataServer.weather[0].icon + ".png";
-
-        let resultDescription = dataServer.weather[0].description;
-        weatherDescriptionMain.innerText = resultDescription.toUpperCase();
-
-
-        temperatureElement.innerHTML = Math.floor(dataServer.main.temp) + " °C" + "...yeah";
-        console.log(temperatureElement)
-        windElement.innerHTML = "Wind blows at " + Math.floor(dataServer.wind.speed) + " mph" + "...wohoo";
-        console.log(windElement)
-        cityName.innerHTML = dataServer.name;
-        humidityElement.innerHTML = "Humidity is by " + dataServer.main.humidity + "% ";
-        console.log(humidityElement)
-
-        setPositionWeatherBox();
-
-    }
-
-    function setPositionWeatherBox() {
-
-        let weatherContainer = document.getElementById("weatherContainer");
-        let weatherContainerHeight = weatherContainer.clientHeigth;
-        let weatherContainerWidth = weatherContainer.clientWidth;
-
-        weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
-        weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/2}px)`;
-        weatherContainer.style.visibility = "visible";
-    }
-
-    document.getElementById("searchButton").addEventListener("click", () => {
-        let searchTerm = document.getElementById("searchInput").value;
-        if (searchTerm)
-            searchWeather(searchTerm);
     })
+}
+
+function init(dataServer) {
+    console.log(dataServer)
+    switch (dataServer.weather[0].main) {
+        case "Clear":
+            document.body.style.backgroundImage = "url('./pictures/clear1.jpg')";
+            break;
+
+        case "Clouds":
+            document.body.style.backgroundImage = "url('./pictures/cloudy.jpg')";
+            break;
+
+        case "Rain":
+        case "Drizzle":
+            document.body.style.backgroundImage = "url('./pictures/rainy.jpg')";
+            break;
+        case "Mist":
+            document.body.style.backgroundImage = "url('./pictures/mist.jpg')";
+            break;
+
+        case "Snow":
+            document.body.style.backgroundImage = "url('./pictures/snowy.jpg')";
+            break;
+
+        case "Thunderstorm":
+            document.body.style.backgroundImage = "url('./pictures/stormy.jpg')";
+            break;
+
+        default:
+            // document.body.style.backgroundImage = "url('./pictures/default1.jpg')";
+            break;
+    }
+    let weatherDescriptionMain = document.getElementById("weatherDescriptionMain")
+    let temperatureElement = document.getElementById("temperatureMain")
+    let humidityElement = document.getElementById("humidity")
+    let windElement = document.getElementById("wind")
+    let cityName = document.getElementById("cityName")
+    let weatherIcon = document.getElementById("iconImg")
+
+    weatherIcon.src = "http://openweathermap.org/img/wn/" + dataServer.weather[0].icon + ".png";
+
+    let resultDescription = dataServer.weather[0].description;
+    weatherDescriptionMain.innerText = resultDescription.toUpperCase();
+
+
+    temperatureElement.innerHTML = Math.floor(dataServer.main.temp) + " °C" + "...yeah";
+    console.log(temperatureElement)
+    windElement.innerHTML = "Wind blows at " + Math.floor(dataServer.wind.speed) + " mph" + "...wohoo";
+    console.log(windElement)
+    cityName.innerHTML = dataServer.name;
+    humidityElement.innerHTML = "Humidity is by " + dataServer.main.humidity + "% ";
+    console.log(humidityElement)
+
+    setPositionWeatherBox();
+
+}
+
+function setPositionWeatherBox() {
+
+    let weatherContainer = document.getElementById("weatherContainer");
+    let weatherContainerHeight = weatherContainer.clientHeigth;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/2}px)`;
+    weatherContainer.style.visibility = "visible";
+}
+
+document.getElementById("searchButton").addEventListener("click", () => {
+    let searchTerm = document.getElementById("searchInput").value;
+    if (searchTerm)
+        searchWeather(searchTerm);
+})
